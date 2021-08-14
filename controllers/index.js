8f1d68ea90d70672354fea41361f24a36d201aa1
SL <samanthl795@gmail.com>
2021-08-13 21:56:49 -0700

    Add GET route for /stats

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const exerciseRoutes = require('./exerciseRoutes');
+const statsRoutes = require('./statsRoutes');
 
 router.use('/exercise', exerciseRoutes);
+router.use('/stats', statsRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
