9d6b88bdc53a77fe98beee68e7bf02edc65a73cf
SL <samanthl795@gmail.com>
2021-08-13 22:40:25 -0700

    Add GET for /api/workouts

--- controllers/api/workoutsRoutes.js
+++ controllers/api/workoutsRoutes.js
@@ -1,3 +1,20 @@
 const router = require('express').Router();
+const mongojs = require("mongojs");
+
+const databaseUrl = "workout";
+const collections = ["workouts"];
+
+const db = mongojs(databaseUrl, collections);
+// Get all workouts in ascending order
+router.get('/', async (req, res) => {
+    db.workouts.find().sort({day: 1}, (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+            console.log(data);
+          res.json(data);
+        }
+      });
+});
 
 module.exports = router;
